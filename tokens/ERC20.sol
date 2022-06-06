7c9900828eee12b7fa5de2977ca57d7e566fa174
Harshini <105817423+crypte-1@users.noreply.github.com>
2022-06-06 15:13:10 +0530

    add decimal function

--- tokens/ERC20.sol
+++ tokens/ERC20.sol
@@ -6,6 +6,10 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contr
 contract MyToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         // Mint 100 tokens to msg.sender
+        //function decimals() public view virtual returns (uint8) {
+        //    return 18;
+        //  }
+        
         _mint(msg.sender, 100 * 10**uint(decimals()));
     }
 }
