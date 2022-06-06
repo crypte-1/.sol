c00014124a4a21eb61aa1eb816e317450dbeaad5
crypte-1 <renukavutukuri1@gmail.com>
2022-06-06 10:30:04 +0530

    Added modifiers in Multi_sig

--- Applications/Multi_sig.sol
+++ Applications/Multi_sig.sol
@@ -18,4 +18,41 @@ contract MultiSigWallet {
     // owners can revoke their aprovance
     event ExecuteTransaction(address indexed owner, uint indexed txIndex);
     // Execute the contract after attaining suffecient approvals
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+    // min num of approvals
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+    // mapping from tx index => owner => bool
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+    // whether mes.sender is in owners list
+    modifier txExists(uint _txIndex) {
+        require(_txIndex < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
 }
