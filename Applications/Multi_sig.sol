27a3ddf6ecca4ae6cce6c095ddadb3ba5195a730
crypte-1 <renukavutukuri1@gmail.com>
2022-06-06 10:41:56 +0530

    function executeTransaction

--- Applications/Multi_sig.sol
+++ Applications/Multi_sig.sol
@@ -114,4 +114,27 @@ contract MultiSigWallet {
 
         emit ConfirmTransaction(msg.sender, _txIndex);
     }
+
+    function executeTransaction(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(
+            transaction.numConfirmations >= numConfirmationsRequired,
+            "cannot execute tx"
+        );
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "tx failed");
+
+        emit ExecuteTransaction(msg.sender, _txIndex);
+    }
 }
