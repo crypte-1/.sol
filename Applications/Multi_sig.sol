578f24b2e46cd6d1a4765dded325c13a83bf8712
crypte-1 <renukavutukuri1@gmail.com>
2022-06-06 10:03:53 +0530

    Added comments

--- Applications/Multi_sig.sol
+++ Applications/Multi_sig.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 contract MultiSigWallet {
     event Deposit(address indexed sender, uint amount, uint balance);
+    // event to trigger as ether deposited
     event SubmitTransaction(
         address indexed owner,
         uint indexed txIndex,
@@ -10,7 +11,11 @@ contract MultiSigWallet {
         uint value,
         bytes data
     );
+    // to show as transaction submited
     event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    // conforming transactions by owers
     event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    // owners can revoke their aprovance
     event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+    // Execute the contract after attaining suffecient approvals
 }
