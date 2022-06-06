06e742c770e2b6fbce531f06cb7719c8c6e6fad3
crypte-1 <renukavutukuri1@gmail.com>
2022-06-06 10:36:49 +0530

    function submitTransaction

--- Applications/Multi_sig.sol
+++ Applications/Multi_sig.sol
@@ -80,4 +80,24 @@ contract MultiSigWallet {
     receive() external payable {
         emit Deposit(msg.sender, msg.value, address(this).balance);
     }
+
+    function submitTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public onlyOwner {
+        uint txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
 }
